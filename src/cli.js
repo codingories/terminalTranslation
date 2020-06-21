20a0263f37bc66d91f19b3305f98e69f26bf46ae
朱骏杰 <ories.zhu@mochain.vip>
2020-06-21 19:12:16 +0800

    开始学习调翻译接口

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,7 @@
-const { Command } = require('commander');
+const {Command} = require('commander');
+const {translate} = require('./main')
+
+// import {translate} from "./main"; // 会报错
 
 
 // const program = new commander.Command();
@@ -6,6 +9,10 @@ const program = new Command();
 // console.log(program)
 program.version('0.0.1')
   .name('fy')
-  .usage('<english>') // <必须选 [可选
-
+  .usage('<English>') // <必须选 [可选
+  .arguments('<English>')
+  .action(function (english) {
+    console.log(english)
+    translate(english)
+  });
 program.parse(process.argv);
